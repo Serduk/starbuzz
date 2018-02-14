4b7fd605807a21148fee08bdbdcc7789f5b10557
Serdiuk Serhii <serduksergey90@gmail.com>
2018-02-14 21:06:56 +0200

    display on main screen all favorites products. add functionality, where user can open favorites drink from favorites

--- app/src/main/java/com/serdyuk/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/serdyuk/starbuzz/TopLevelActivity.java
@@ -2,12 +2,21 @@ package com.serdyuk.starbuzz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.view.View;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class TopLevelActivity extends Activity {
+    private SQLiteDatabase database;
+    private Cursor favoritesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,8 @@ public class TopLevelActivity extends Activity {
                                     int position,
                                     long l) {
                 if (position == 0) {
-                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                    Intent intent = new Intent(TopLevelActivity.this,
+                            DrinkCategoryActivity.class);
                     startActivity(intent);
                 } else if (position == 1) {
                     System.out.println("Clicked on button FOOD");
@@ -35,6 +45,56 @@ public class TopLevelActivity extends Activity {
         //        Add Listener to list view in activity
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
+
+        ListView listFavorites = (ListView) findViewById(R.id.list_favorites);
+
+        /*
+        * Connect to database and try get all favorites drinks
+        * */
+        try {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            database = starbuzzDatabaseHelper.getReadableDatabase();
+            favoritesCursor = database.query("DRINK",
+                    new String[] {"_id", "NAME"},
+                    "FAVORITE = 1",
+                    null, null, null, null);
+
+            /*
+             * Set all names of favorites drinks on screen
+             * */
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(TopLevelActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1}, 0);
+
+            listFavorites.setAdapter(favoriteAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        /*
+        * Switching to drinkActivity after choosing drink in favorites
+        * */
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                /*
+                * If user choose one of this variants of favorites list
+                * Create Intent for start DrinkActivity an send identificator of drink
+                * */
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int)id);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        database.close();
     }
 
     public void tapOnLogo(View view) {
