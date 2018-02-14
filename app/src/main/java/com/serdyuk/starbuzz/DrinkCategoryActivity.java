94383d1d89c75ac5254631927f32f2bf2fcfc408
Serdiuk Serhii <serduksergey90@gmail.com>
2018-02-14 19:29:14 +0200

    connect database to DrinkCategoryActivity

--- app/src/main/java/com/serdyuk/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/serdyuk/starbuzz/DrinkCategoryActivity.java
@@ -2,33 +2,66 @@ package com.serdyuk.starbuzz;
 
 import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends ListActivity {
+    private SQLiteDatabase sqLiteDatabase;
+    private Cursor cursor;
+
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
 
         ListView listDrinks = getListView();
 
-        /*
-         * should be send 3 params:
-         *      1. Current activity
-         *      2. layout of current activity
-         *      3. Array for displaying in activity
-         * */
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>(
-                this,
-                android.R.layout.simple_list_item_1,
-                Drink.drinks);
+        try {
+            /*
+            * Get links to DB
+            * */
+            System.out.println("Try connect to database");
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            sqLiteDatabase = starbuzzDatabaseHelper.getReadableDatabase();
+
+
+            /*
+            * Cursor crating
+            * */
+            System.out.println("Cursor creating");
+            cursor = sqLiteDatabase.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            CursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_expandable_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[] {android.R.id.text1},
+                    0);
 
         /*
          * Array adapter connect with list view by mthod setAdapter() in class ListView
          * */
-        listDrinks.setAdapter(listAdapter);
+            listDrinks.setAdapter(listAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        sqLiteDatabase.close();
     }
 
     @Override
